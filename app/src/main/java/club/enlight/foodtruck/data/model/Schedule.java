ef0967f44b6d2015385372a9a75f9685deb622ac
newsatit <newsatit97@gmail.com>
2017-11-06 00:11:54 -0600

    implement Schedule.java and add Time.java

--- app/src/main/java/club/enlight/foodtruck/data/model/Schedule.java
+++ app/src/main/java/club/enlight/foodtruck/data/model/Schedule.java
@@ -3,25 +3,37 @@ package club.enlight.foodtruck.data.model;
 /**
  * Schedule containing food truck hours of operation
  *
- * TODO: implement data structure for storing and retrieving schedule information
  *
  * Created by jzmai on 10/17/17.
  */
 public class Schedule {
 
-    // TODO: implement constructor
+    private Time[] openingTime;
+    private Time[] closingTime;
+
     public Schedule(){
-
-
+        openingTime = new Time[7];
+        closingTime = new Time[7];
+        for(int i = 0; i < 7; i++) {
+            openingTime[i] = null;
+            closingTime[i] = null;
+        }
     }
 
-    // TODO: implement setter and getter functions
-    public int getScheduleByDay(){
-
-
-        return 0;
+    public void setOpeningTimeByDay(int day, int hour, int minute) {
+        openingTime[day] = new Time(hour, minute);
     }
 
+    public void setClosingTimeTimeByDay(int day, int hour, int minute) {
+        closingTime[day] = new Time(hour, minute);
+    }
 
+    public Time getOpeningTimeByDay(int day) {
+        return openingTime[day];
+    }
+
+    public Time getClosingTimeByDay(int day) {
+        return closingTime[day];
+    }
 
 }
