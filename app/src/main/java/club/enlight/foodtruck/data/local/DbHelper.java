d8ef4f097f252d414b36f3772fb1c415e6e5f49b
James Z Mai <jzmai@wisc.edu>
2017-11-07 18:48:42 -0600

    Added setters and getter for Food Cart. Added skeloton methods to DbHelper

--- app/src/main/java/club/enlight/foodtruck/data/local/DbHelper.java
+++ app/src/main/java/club/enlight/foodtruck/data/local/DbHelper.java
@@ -12,15 +12,46 @@ public class DbHelper {
 
 
     /**
+     * Takes FoodTruck object and pushes it to the database
+     *
+     * @param truck truck to push to database
+     * @return
+     */
+    public static int pushFoodTruck(FoodTruck truck){
+
+        return -1;
+    }
+
+    /**
+     * Takes FoodTruck object and pulls data from
+     *
+     * @param truck
+     * @return
+     */
+    public static FoodTruck getFoodTruck(FoodTruck truck){
+        // update data in FoodTruck object from database
+
+        if( truck.getId() < 0 ) return null;
+
+        return truck;
+    }
+
+    public static FoodTruck getFoodTruck(int id){
+        FoodTruck truck = new FoodTruck(id);
+        return getFoodTruck(truck);
+    }
+
+    /**
      * Constructs FoodTruck object from data queried from database based on id parameter
      *
      * @param id Unique ID of FoodTruck to query from database
      * @return FoodTruck with matching ID. Null if no truck exists
      */
-    public FoodTruck getTruckById(int id){
+    public static FoodTruck getTruckById(int id){
         FoodTruck result = new FoodTruck();
-
         return result;
     }
+
+
 
 }
