07880aaec07ccbf0f508549237d5f91e524937f4
James Z Mai <jzmai@wisc.edu>
2017-11-28 18:10:37 -0600

    Fixed Typo
    Fixed Typo

--- app/src/main/java/club/enlight/foodtruck/data/local/DbHelper.java
+++ app/src/main/java/club/enlight/foodtruck/data/local/DbHelper.java
@@ -4,7 +4,7 @@ import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.content.Context;
-import android .database.Cursor;
+import android.database.Cursor;
 
 import java.util.HashMap;
 
